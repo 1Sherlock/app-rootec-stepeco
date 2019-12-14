a4fa57cc57d8a7c8377edb83eedc0c7a61c7b02d
Nizom Muhammadov <nizom702@gmail.com>
2019-12-14 18:27:44 +0500

    getting location

--- app-rootec-stepeco-hardware/app-rootec-stepeco-hardware/app-rootec-stepeco-hardware.ino
+++ app-rootec-stepeco-hardware/app-rootec-stepeco-hardware/app-rootec-stepeco-hardware.ino
@@ -1,26 +1,39 @@
 #include <DHT.h>
+#include <TinyGPS++.h>
+#include <SoftwareSerial.h>
 
 #define DHTPIN 2   
 
+int RXPin = 4;
+int TXPin = 3;
+int GPSBaud = 9600;
+
 #define DHTTYPE DHT22 
 DHT dht(DHTPIN, DHTTYPE);
+TinyGPSPlus gps;
 
+SoftwareSerial gpsSerial(TXPin, RXPin);
 
 
 int chk;
 float hum;  
 float temp;
 int quality;
+float lattitude;
+float longitude;
+
 
 void setup()
 {
     Serial.begin(9600);
+      gpsSerial.begin(GPSBaud);
   dht.begin();
 
 }
 
 void loop()
 {
+  getLocation();
   calcQuality();
   calcTemp();
 }
@@ -38,10 +51,31 @@ void calcTemp(){
     Serial.print("Havo sifati = ");
     Serial.print(quality);
     Serial.println("*PPM");
+    Serial.print("Latitude: ");
+    Serial.print(lattitude);
+     Serial.print("Longitude: ");
+      Serial.print(longitude);
     delay(2000); 
 }
 
 void calcQuality(){
    quality = analogRead(A0);
     delay(1000);
+}
+
+void getLocation()
+{
+  if (gps.location.isValid())
+  {
+   
+    lattitude = gps.location.lat();
+   
+    longitude = gps.location.lng();
+  }
+  else
+  {
+    Serial.println("Location: Not Available");
+  }
+  
+  delay(1000);
 }
