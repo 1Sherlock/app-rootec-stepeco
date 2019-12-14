724eb49fd0974ce050191eaf08cfc76f0c4d34fc
Kabirjonova Muslimakhon <princess7sweetie@gmail.com>
2019-12-14 15:47:30 +0500

    Add map

--- app/src/main/java/com/rootec/stepeco/MainFragment.kt
+++ app/src/main/java/com/rootec/stepeco/MainFragment.kt
@@ -6,6 +6,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.yandex.mapkit.MapKitFactory
+import kotlinx.android.synthetic.main.fragment_main.*
 
 class MainFragment : Fragment() {
 
@@ -13,8 +15,22 @@ class MainFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
+        MapKitFactory.setApiKey("11159977-5dee-4b7f-a199-9b58fc6b84bb")
+        MapKitFactory.initialize(activity)
+
         return inflater.inflate(R.layout.fragment_main, container, false)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        map.onStart()
+        MapKitFactory.getInstance().onStart()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        map.onStop()
+        MapKitFactory.getInstance().onStop()
     }
 
 
