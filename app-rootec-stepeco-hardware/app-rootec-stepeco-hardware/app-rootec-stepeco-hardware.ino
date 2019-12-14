2498f5e10c4b7839d12c807df56853ad7b9f26d4
Nizom Muhammadov <nizom702@gmail.com>
2019-12-14 17:37:43 +0500

    get temperature and humidity

--- app-rootec-stepeco-hardware/app-rootec-stepeco-hardware/app-rootec-stepeco-hardware.ino
+++ app-rootec-stepeco-hardware/app-rootec-stepeco-hardware/app-rootec-stepeco-hardware.ino
@@ -1,9 +1,36 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <DHT.h>
+
+#define DHTPIN 2   
+#define DHTTYPE DHT22 
+DHT dht(DHTPIN, DHTTYPE);
+
+
+
+int chk;
+float hum;  
+float temp;
+
+void setup()
+{
+    Serial.begin(9600);
+  dht.begin();
 
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop()
+{
+  calcTemp();
+}
 
+
+void calcTemp(){
+  hum = dht.readHumidity();
+    temp= dht.readTemperature();
+    
+    Serial.print("Namlik: ");
+    Serial.print(hum);
+    Serial.print(" %, Harorat: ");
+    Serial.print(temp);
+    Serial.println(" °C");
+    delay(2000); 
 }
