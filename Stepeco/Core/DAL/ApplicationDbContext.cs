9f9d662a8f281cc7cb5ae9e95a91ea16de46853d
imicoderone <imicoderone@gmail.com>
2019-12-14 16:53:50 +0600

    Add Base Repository and Context

---
+++ Stepeco/Core/DAL/ApplicationDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Stepeco.Core.DAL.Entities;
+using Stepeco.Core.DAL.Mappings;
+
+namespace Stepeco.Core.DAL
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<EnvironmentRecord> EnvironmentRecords { get; set; }
+        public DbSet<Step> Steps { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.ApplyConfiguration(new EnvironmentRecordMap());
+            builder.ApplyConfiguration(new StepMap());
+        }
+    }
+}
